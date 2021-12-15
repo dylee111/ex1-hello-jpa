14546b0146d418dced6ea04876ea39fedd66a734
DDDZZZ <dylee0568@gmail.com>
2021-12-15 22:55:50 +0900

    2021.12.15 JPA - 엔티티 설계 JPA - 단반향 연관관계

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,15 +1,21 @@
-package hellejpa;
+package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity // JPA 처음 로딩될 때 JPA를 사용하는 객체로 인식하고 관리함.
 //@Table(name = "USER") // 만약 DB의 테이블 이름이 USER로 되어있다면 @Table의 name 속성을 통해 매핑
+@SequenceGenerator(name="MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 매핑할 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -25,5 +31,13 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
